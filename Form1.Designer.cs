0ef69a92f8f61f3e75a16b6988b2b7b092811ea2
shinymajesty <rootgives@gmail.com>
2025-05-01 01:05:32 +0200

    #Added basic game functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            bird = new PictureBox();
+            pipeBot = new Panel();
+            pipeTop = new Panel();
+            gameTimer = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)bird).BeginInit();
+            SuspendLayout();
+            // 
+            // bird
+            // 
+            bird.BackColor = Color.Transparent;
+            bird.Image = Properties.Resources.bird_flat;
+            bird.Location = new Point(128, 235);
+            bird.Name = "bird";
+            bird.Size = new Size(152, 129);
+            bird.SizeMode = PictureBoxSizeMode.StretchImage;
+            bird.TabIndex = 0;
+            bird.TabStop = false;
+            // 
+            // pipeBot
+            // 
+            pipeBot.BackColor = Color.ForestGreen;
+            pipeBot.Location = new Point(1016, 490);
+            pipeBot.Name = "pipeBot";
+            pipeBot.Size = new Size(200, 443);
+            pipeBot.TabIndex = 5;
+            // 
+            // pipeTop
+            // 
+            pipeTop.BackColor = Color.ForestGreen;
+            pipeTop.Location = new Point(1016, -3);
+            pipeTop.Name = "pipeTop";
+            pipeTop.Size = new Size(200, 251);
+            pipeTop.TabIndex = 6;
+            // 
+            // gameTimer
+            // 
+            gameTimer.Interval = 10;
+            gameTimer.Tick += gameTimer_Tick;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(107, 48);
+            button1.TabIndex = 7;
+            button1.TabStop = false;
+            button1.Text = "Start";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += buttonStart_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Highlight;
+            ClientSize = new Size(1549, 769);
+            Controls.Add(button1);
+            Controls.Add(pipeTop);
+            Controls.Add(pipeBot);
+            Controls.Add(bird);
+            Name = "Form1";
+            Text = "Form1";
+            KeyDown += Form1_KeyDown;
+            ((System.ComponentModel.ISupportInitialize)bird).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox bird;
+        private Panel pipeBot;
+        private Panel pipeTop;
+        private System.Windows.Forms.Timer gameTimer;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,75 @@
+using System.Drawing.Drawing2D;
+
 namespace Game
 {
     public partial class Form1 : Form
     {
+
+        public bool birdIsJumping = false;
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            pipeBot.Left -= 10;
+            pipeTop.Left -= 10;
+
+            if (pipeBot.Left < -pipeBot.Width)
+            {
+                pipeBot.Left = this.ClientSize.Width;
+                pipeTop.Left = this.ClientSize.Width;
+            }
+            if(!birdIsJumping)
+                bird.Top += 5;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            gameTimer.Start();
+            button1.Enabled = false;
+            this.Focus();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+         {
+            if(e.KeyCode == Keys.Space)
+            {
+                IncreaseBirdHeight();
+                
+            }
+        }
+        async Task IncreaseBirdHeight()
+        {
+            birdIsJumping = true;
+            
+            bird.Image = RotateImage(new Bitmap(bird.Image), -30);
+
+
+
+            foreach (var i in Enumerable.Range(0, 10))
+            {
+                bird.Top -= 10;
+                await Task.Delay(1);
+            }
+            birdIsJumping = false;
+            await Task.Delay(100);
+            bird.Image = RotateImage(new Bitmap(bird.Image), 30);
+        }
+        public Bitmap RotateImage(Bitmap bmp, float angle)
+        {
+            Bitmap result = new Bitmap(bmp.Width, bmp.Height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.TranslateTransform(bmp.Width / 2, bmp.Height / 2);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-bmp.Width / 2, -bmp.Height / 2);
+                g.DrawImage(bmp, new Point(0, 0));
+            }
+            return result;
+        }
+
     }
 }
